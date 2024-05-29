ff571697ceb7f137899673592c2278f1d1a99e71
felix45 <fatonoh@gmail.com>
2024-05-30 02:03:07 +0300

    Add about section to homepage

---
+++ src/components/About.jsx
@@ -0,0 +1,13 @@
+const About = () => (
+  <section className="container mx-auto p-5">
+    <h1 className="text-3xl font-black mb-3 text-dist border-b pb-3">About Pixar.</h1>
+    <p className="first-letter:float-left first-letter:text-7xl first-letter:pr-2 first-letter:text-dist first-letter:font-bold text-justify">
+      Pixar provides a convenient and enjoyable way for users to preserve and share
+      their cherished moments with friends, family, and the world. The app&apos;s interface
+      is clean and user-friendly, designed with soft pastel colors to evoke a
+      sense of nostalgia and warmth.
+    </p>
+  </section>
+);
+
+export default About;
