1a655af684dd1bed89dfccf910a37f6e12592062
felix45 <fatonoh@gmail.com>
2024-05-30 03:10:36 +0300

    Add gallery component to the homepage

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import NavBar from './components/NavBar';
 import Footer from './components/Footer';
 import Carousel from './components/Carousel';
 import About from './components/About';
+import Gallery from './components/Gallery';
 
 const App = () => (
   <main className="min-h-full">
@@ -9,6 +10,7 @@ const App = () => (
     <div className="min-h-screen">
       <Carousel />
       <About />
+      <Gallery />
     </div>
     <Footer />
   </main>
