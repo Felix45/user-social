36b14d330cc70a6198e6cac0cf4d1050a486cf09
felix45 <fatonoh@gmail.com>
2024-05-30 18:52:20 +0300

    Add app homepage

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import About from '../components/About';
+import Carousel from '../components/Carousel';
+import Gallery from '../components/Gallery';
+
+const Home = () => (
+  <>
+    <Carousel />
+    <About />
+    <Gallery />
+  </>
+);
+
+export default Home;
