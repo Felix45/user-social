338e68a9af0bccb38a113b0dcc1eee994e94f27d
felix45 <fatonoh@gmail.com>
2024-05-30 18:50:48 +0300

    Add application routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,17 @@
-import NavBar from './components/NavBar';
-import Footer from './components/Footer';
-import Carousel from './components/Carousel';
-import About from './components/About';
-import Gallery from './components/Gallery';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Layout from './Layout';
 
 const App = () => (
-  <main className="min-h-full">
-    <NavBar />
-    <div className="min-h-screen">
-      <Carousel />
-      <About />
-      <Gallery />
-    </div>
-    <Footer />
-  </main>
+  <Router>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="login" element={<Login />} />
+      </Route>
+    </Routes>
+  </Router>
 );
 
 export default App;
