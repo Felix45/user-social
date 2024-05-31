183071255ee59650179c9806d9651fa13566af1e
felix45 <fatonoh@gmail.com>
2024-05-31 19:18:53 +0300

    Add login and logout functionality to navigation menu

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,18 +1,27 @@
 import { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import { CgMenuRightAlt } from 'react-icons/cg';
 import { IoClose } from 'react-icons/io5';
 import { v4 as uuidv4 } from 'uuid';
+import { googleLogout } from '@react-oauth/google';
+import { NavLink } from 'react-router-dom';
+import Login from './Login';
+import { logout } from '../redux/slices/authSlice';
+import { setProfile } from '../redux/slices/profileSlice';
 
 const NavBar = () => {
   const urls = {
-    Home: '/',
+    Pixar: '/',
     About: '/about',
     People: '/people',
     Albums: '/albums',
   };
 
+  const dispatch = useDispatch();
+
   const [open, setOpen] = useState(false);
   const [mobile, setMobile] = useState('hidden');
+  const { profile } = useSelector((state) => state.profile);
 
   const handleMenu = (close) => {
     setOpen(close);
@@ -23,17 +32,34 @@ const NavBar = () => {
     }
   };
 
+  const logOut = () => {
+    googleLogout();
+    dispatch(logout());
+    dispatch(setProfile(null));
+  };
+
   return (
 
     <header className="bg-black p-5 shadow-lg">
       <div className="container flex mx-auto justify-between items-center">
-        <div className="text-white font-bold text-2xl">
-          <span>Pixar.</span>
-        </div>
+        <NavLink to="/" className="text-white font-bold text-2xl">
+          <span to="/">Pixar.</span>
+        </NavLink>
         <nav className="flex justify-end md:justify-center">
-          <ul className={`${mobile} z-20 flex flex-col shadow-lg absolute bg-black w-3/4 top-20 right-0 bottom-0 text-white md:shadow-none md:items-center md:flex md:mr-5 md:flex-row md:static md:top-0`}>
+          <ul className={`${mobile} z-20 flex flex-col shadow-lg absolute bg-black w-3/4 top-16 right-0 bottom-0 text-white md:shadow-none md:items-center md:flex md:mr-5 md:flex-row md:static md:top-0`}>
             {
-          Object.keys(urls).map((item) => <li key={uuidv4()} className="p-4 border-dotted border-b md:border-0 md:px-4">{item}</li>)
+          Object.keys(urls).map((item) => (
+            <li key={uuidv4()} className="p-4 font-bold border-dotted border-b md:border-0 md:px-1">
+              <NavLink to={urls[item]} className="block">{item}</NavLink>
+            </li>
+          ))
+         }
+            {
+          profile && (
+          <li className="p-4 font-bold border-dotted border-b md:border-0 md:hidden md:px-1">
+            <NavLink onClick={() => logOut()} className="block">Logout</NavLink>
+          </li>
+          )
          }
           </ul>
 
@@ -43,9 +69,7 @@ const NavBar = () => {
                 : <CgMenuRightAlt onClick={() => handleMenu(true)} /> }
             </li>
             <li>
-              <button type="button" className="px-4 py-2 bg-white text-black font-bold rounded-md">
-                Sign In
-              </button>
+              <Login />
             </li>
           </ul>
         </nav>
