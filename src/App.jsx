c64674134fee8f5f882956712d228ad76bbe2205
felix45 <fatonoh@gmail.com>
2024-05-31 19:09:42 +0300

    Wrap App with react-redux store provider  and persist gate components

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,24 @@
+import { Provider } from 'react-redux';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { persistStore } from 'redux-persist';
+import { PersistGate } from 'redux-persist/integration/react';
 import Home from './pages/Home';
-import Login from './pages/Login';
 import Layout from './Layout';
+import store from './redux/store';
+
+const persistor = persistStore(store);
 
 const App = () => (
   <Router>
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route path="/" element={<Home />} />
-        <Route path="login" element={<Login />} />
-      </Route>
-    </Routes>
+    <Provider store={store}>
+      <PersistGate persistor={persistor}>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route path="/" element={<Home />} />
+          </Route>
+        </Routes>
+      </PersistGate>
+    </Provider>
   </Router>
 );
 
