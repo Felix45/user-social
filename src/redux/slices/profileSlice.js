ff9769465d0d146aa14dfdad4b6c5806965a4e01
felix45 <fatonoh@gmail.com>
2024-05-31 19:07:47 +0300

    Add set user profile redux slice

---
+++ src/redux/slices/profileSlice.js
@@ -0,0 +1,45 @@
+import axios from 'axios';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+
+const initialState = {
+  isLoading: false,
+  isError: false,
+  profile: null,
+};
+
+export const userProfileThunk = createAsyncThunk(
+  'user/profile',
+  async (user) => {
+    const profile = await axios.get(`https://www.googleapis.com/oauth2/v1/userinfo?access_token=${user.access_token}`, {
+      headers: {
+        Authorization: `Bearer ${user.access_token}`,
+        Accept: 'application/json',
+      },
+    }).then((response) => response.data)
+      .catch((error) => error);
+
+    return profile;
+  },
+);
+
+const profileSlice = createSlice({
+  name: 'user',
+  initialState,
+  reducers: {
+
+    setProfile(state, action) {
+      return {
+        ...state,
+        profile: action.payload,
+      };
+    },
+  },
+  extraReducers: (builder) => {
+    builder.addCase(userProfileThunk.fulfilled, (state, action) => {
+      state.profile = action.payload;
+    });
+  },
+});
+
+export const { setProfile } = profileSlice.actions;
+export default profileSlice.reducer;
