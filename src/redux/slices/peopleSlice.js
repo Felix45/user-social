a8ea39030f66d77433ab46969122e8326a7cf94a
felix45 <fatonoh@gmail.com>
2024-06-02 10:11:37 +0300

    Add a slice to fetch all the users from API

---
+++ src/redux/slices/peopleSlice.js
@@ -0,0 +1,33 @@
+import axios from 'axios';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+
+const initialState = {
+  isLoading: false,
+  isError: false,
+  people: null,
+};
+
+export const fetchPeopleThunk = createAsyncThunk(
+  'people/list',
+  async () => {
+    const users = await axios.get('https://jsonplaceholder.typicode.com/users').then((response) => response.data)
+      .catch((error) => error);
+
+    return users;
+  },
+);
+
+const peopleSlice = createSlice({
+  name: 'people',
+  initialState,
+  reducers: {
+
+  },
+  extraReducers: (builder) => {
+    builder.addCase(fetchPeopleThunk.fulfilled, (state, action) => {
+      state.people = action.payload;
+    });
+  },
+});
+
+export default peopleSlice.reducer;
