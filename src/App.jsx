6b400a8d20364c2e0e6c2a74dca0814df210a2a2
felix45 <fatonoh@gmail.com>
2024-06-02 18:03:23 +0300

    Add route that leads to user details page

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { persistStore } from 'redux-persist';
 import { PersistGate } from 'redux-persist/integration/react';
 import Landing from './pages/Landing';
 import Home from './pages/Home';
+import UserDetails from './pages/UserDetails';
 import Layout from './Layout';
 import store from './redux/store';
 
@@ -17,6 +18,7 @@ const App = () => (
           <Route path="/" element={<Layout />}>
             <Route path="/" element={<Landing />} />
             <Route path="/users" element={<Home />} />
+            <Route path="/users/:id" element={<UserDetails />} />
           </Route>
         </Routes>
       </PersistGate>
