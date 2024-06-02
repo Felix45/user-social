2c584c837360504203f126422a1cef51f201ed7d
felix45 <fatonoh@gmail.com>
2024-06-02 18:01:34 +0300

    Insert Profile Pic component in User component

--- src/components/parts/User.jsx
+++ src/components/parts/User.jsx
@@ -1,12 +1,10 @@
-import { LuUser2 } from 'react-icons/lu';
 import { NavLink } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import ProfilePic from './ProfilePic';
 
 const User = ({ id, name, albums }) => (
   <NavLink to={`/users/${id}`} key={id} className="flex mb-5 border-b border-dotted text-sm md:shadow-2xl md:rounded-xl md:py-5 md:border-none md:mb-5 md:flex-col">
-    <div className="flex items-center bg-gray-100 mb-3 rounded-full p-5 w-[90px] h-[90px] md:mx-auto">
-      <LuUser2 size={70} className="text-dist md:mx-auto md:mb-5" />
-    </div>
+    <ProfilePic width="90px" height="90px" />
     <div className="ml-3 md:text-center md:mx-auto">
       <h2 className="text-xl font-bold">{name}</h2>
       <div className="flex items-end md:justify-center">
