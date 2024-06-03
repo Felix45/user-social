58c1e2c4754a17212258ebdd8dbce858321759ab
felix45 <fatonoh@gmail.com>
2024-06-03 14:30:24 +0300

    Add fetch album details redux slice to get selected album info

---
+++ src/redux/slices/albumDetailSlice.js
@@ -0,0 +1,33 @@
+import axios from 'axios';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+
+const initialState = {
+  isLoading: false,
+  isError: false,
+  album: null,
+};
+
+export const fetchAlbumDetailThunk = createAsyncThunk(
+  'albumDetail/item',
+  async (id) => {
+    const album = await axios.get(`https://jsonplaceholder.typicode.com/albums/${id}`).then((response) => response.data)
+      .catch((error) => error);
+
+    return album;
+  },
+);
+
+const albumDetailSlice = createSlice({
+  name: 'albumDetail',
+  initialState,
+  reducers: {
+
+  },
+  extraReducers: (builder) => {
+    builder.addCase(fetchAlbumDetailThunk.fulfilled, (state, action) => {
+      state.album = action.payload;
+    });
+  },
+});
+
+export default albumDetailSlice.reducer;
