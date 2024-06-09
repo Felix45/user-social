8392fe2c5364165282853ca4626a88f8f4e920b6
felix45 <fatonoh@gmail.com>
2024-06-09 07:03:52 +0300

    Fix broken image links from source.unsplash.com

--- src/data/photos.js
+++ src/data/photos.js
@@ -3,30 +3,30 @@
  */
 const breakpoints = [1080, 640, 384, 256, 128, 96, 64, 48];
 
-const unsplashLink = (id, width, height) => `https://source.unsplash.com/${id}/${width}x${height}`;
+const unsplashLink = (id, width) => `https://assets.react-photo-album.com/_next/image?url=%2F_next%2Fstatic%2Fmedia%2F${id}&w=${width}&q=75`;
 
 const unsplashPhotos = [
-  { id: '8gVv6nxq6gY', width: 1080, height: 800 },
-  { id: 'Dhmn6ete6g8', width: 1080, height: 1620 },
-  { id: 'RkBTPqPEGDo', width: 1080, height: 720 },
-  { id: 'Yizrl9N_eDA', width: 1080, height: 721 },
-  { id: 'KG3TyFi0iTU', width: 1080, height: 1620 },
-  { id: 'Jztmx9yqjBw', width: 1080, height: 607 },
-  { id: '-heLWtuAN3c', width: 1080, height: 608 },
-  { id: 'xOigCUcFdA8', width: 1080, height: 720 },
-  { id: '1azAjl8FTnU', width: 1080, height: 1549 },
-  { id: 'ALrCdq-ui_Q', width: 1080, height: 720 },
-  { id: 'twukN12EN7c', width: 1080, height: 694 },
-  { id: '9UjEyzA6pP4', width: 1080, height: 1620 },
-  { id: 'sEXGgun3ZiE', width: 1080, height: 720 },
-  { id: 'S-cdwrx-YuQ', width: 1080, height: 1440 },
-  { id: 'q-motCAvPBM', width: 1080, height: 1620 },
-  { id: 'Xn4L310ztMU', width: 1080, height: 810 },
-  { id: 'iMchCC-3_fE', width: 1080, height: 610 },
-  { id: 'X48pUOPKf7A', width: 1080, height: 160 },
-  { id: 'GbLS6YVXj0U', width: 1080, height: 810 },
-  { id: '9CRd1J1rEOM', width: 1080, height: 720 },
-  { id: 'xKhtkhc9HbQ', width: 1080, height: 1440 },
+  { id: 'image01.018d1d35.jpg', width: 1080, height: 800 },
+  { id: 'image02.cf33eff7.jpg', width: 1080, height: 1620 },
+  { id: 'image03.cdc32b45.jpg', width: 1080, height: 720 },
+  { id: 'image04.9a1f6335.jpg', width: 1080, height: 721 },
+  { id: 'image05.d7ef12b4.jpg', width: 1080, height: 1620 },
+  { id: 'image06.4ab952e3.jpg', width: 1080, height: 608 },
+  { id: 'image07.ac608196.jpg', width: 1080, height: 607 },
+  { id: 'image08.95e095b5.jpg', width: 1080, height: 720 },
+  { id: 'image09.fa6c4764.jpg', width: 1080, height: 1549 },
+  { id: 'image10.411ea655.jpg', width: 1080, height: 720 },
+  { id: 'image11.f3ea483a.jpg', width: 1080, height: 694 },
+  { id: 'image12.5a9347ea.jpg', width: 1080, height: 1620 },
+  { id: 'image13.ce46dd98.jpg', width: 1080, height: 720 },
+  { id: 'image14.68b2812c.jpg', width: 1080, height: 1440 },
+  { id: 'image15.4461facf.jpg', width: 1080, height: 1620 },
+  { id: 'image16.5ad17d8b.jpg', width: 1080, height: 810 },
+  { id: 'image17.a242e897.jpg', width: 1080, height: 610 },
+  { id: 'image18.0479bde8.jpg', width: 1080, height: 160 },
+  { id: 'image19.ab7b61f4.jpg', width: 1080, height: 810 },
+  { id: 'image20.f62571df.jpg', width: 1080, height: 720 },
+  { id: 'image21.14c9bee0.jpg', width: 1080, height: 1440 },
 ];
 
 const photos = unsplashPhotos.map((photo) => ({
